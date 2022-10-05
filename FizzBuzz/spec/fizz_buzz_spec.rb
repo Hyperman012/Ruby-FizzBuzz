ee7fa6e5823a1cb82567cfb888c2ce313e3ec6e1
Adam Whaley <adam@industriallogic.com>
2022-10-05 08:15:45 -0500

    remove version from minitest in gemfile

--- FizzBuzz/spec/fizz_buzz_spec.rb
+++ FizzBuzz/spec/fizz_buzz_spec.rb
@@ -4,4 +4,5 @@ require_relative '../fizz_buzz'
 describe FizzBuzz do
   it 'does something' do
   end
+
 end
--- Gemfile
+++ Gemfile
@@ -1,2 +1,2 @@
 source 'https://rubygems.org'
-gem 'minitest', '~> 5.16.0'
+gem 'minitest'
